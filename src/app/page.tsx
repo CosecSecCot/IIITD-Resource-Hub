e357e8793e8612728bdec9dd5ce20380d3819501
dhruvlearner-05 <dhruv23198@iiitd.ac.in>
2025-04-12 17:26:37 +0530

    Updated home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <div className="w-full h-[80vh]">Hellow!</div>
+  return (
+    <div className="w-full h-[80vh] flex flex-col items-center justify-center text-center">
+      <h1 className="text-6xl font-bold">IIITD</h1>
+      <p className="text-6xl font-bold">RESOURCE-HUB</p>
+    </div>
+  );
 }
